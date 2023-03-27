c515098594b6f87c9726223c76c4586481ad4f17
BrunoSouza <brunowebsouza@gmail.com>
2023-03-27 11:38:51 -0300

    27/03

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,18 +1,13 @@
 const {Router}= require('express')
 const routes= Router()
-const PrincipalRoutes= require('./principal.routes')
-const DrinksRoutes= require('./drinks.routes')
-const DessertRoutes= require('./dessert.routes')
+
 const SessionsRoutes= require('./sessions.routes')
 const UsersRoutes= require('./users.routes')
 const DishRoutes= require('./dishes.routes')
 
 
 routes.use("/users",UsersRoutes)
-routes.use("/principals",PrincipalRoutes)
 routes.use("/sessions",SessionsRoutes)
-routes.use("/drinks",DrinksRoutes)
-routes.use("/desserts",DessertRoutes)
 routes.use("/dishes", DishRoutes)
 
 module.exports= routes
